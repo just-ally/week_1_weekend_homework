4a262bcc23eeea72b4ae65059323c8b15143ea7d
Ally Conway <avconway@hotmail.co.uk>
2018-11-16 12:38:32 +0000

    Adds up to test 10

--- pet_shop.rb
+++ pet_shop.rb
@@ -9,3 +9,33 @@ end
 def add_or_remove_cash(pet_shop, amount)
   pet_shop[:admin][:total_cash] += amount
 end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, number_of_pets_sold)
+  pet_shop[:admin][:pets_sold] += number_of_pets_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed_to_search)
+  pet_breed = []
+  for pet in pet_shop[:pets]
+    if pet[:breed].include? breed_to_search
+      pet_breed.push(pet)
+    end
+  end
+  return pet_breed
+end
+
+def find_pet_by_name(pet_shop, name_to_find)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name_to_find
+      return pet
+    end
+  end
+end
